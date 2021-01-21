e3df06ecf0628a267fbc2e289b029ae30529be53
Jawad Mufti <38205301+Jawad-Mufti@users.noreply.github.com>
2021-01-22 00:59:28 +0100

    Update template-opencv.cpp

--- woreking with records/cpp-opencv/src/template-opencv.cpp
+++ woreking with records/cpp-opencv/src/template-opencv.cpp
@@ -136,6 +136,9 @@ int32_t main(int32_t argc, char **argv) {
                 sharedMemory->lock();
             
             }
+         
+            cv::Point2f corners[4];
+
          oundingBox.points(corners);
          cv::line(img, corners[1], corners[2], cv::Scalar(0, 0, 256));
                     cv::line(img, corners[2], corners[3], cv::Scalar(0, 0, 256));
