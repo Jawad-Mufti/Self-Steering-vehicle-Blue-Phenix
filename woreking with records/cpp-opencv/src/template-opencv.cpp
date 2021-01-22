e5ac7fcffdd3e872e87ce03b59d2b690bdff4f0b
Jawad Mufti <38205301+Jawad-Mufti@users.noreply.github.com>
2021-01-22 01:01:55 +0100

    Update template-opencv.cpp

--- woreking with records/cpp-opencv/src/template-opencv.cpp
+++ woreking with records/cpp-opencv/src/template-opencv.cpp
@@ -137,6 +137,8 @@ int32_t main(int32_t argc, char **argv) {
             
             }
          
+               //rectangle( contourImage, boundRect[j].tl(), boundRect[j].br(), color, 2 );
+
             cv::Point2f corners[4];
 
          oundingBox.points(corners);
